9ef6d721b8ea1bc3ec3e022490f0feb8ba91a392
ChrFr <christoph.franke@web.de>
2017-09-15 16:49:35 +0200

    Update: playing around with plotly diagrams

--- repair/apps/study_area/views.py
+++ repair/apps/study_area/views.py
@@ -1,31 +1,44 @@
 from django.http import HttpResponse
 from django.template import loader
 from django.views.generic import TemplateView
-import plotly.offline as opy
-import plotly.graph_objs as go
+from plotly.offline import plot
+from plotly.graph_objs import (Scatter, Marker, Histogram2dContour, Contours,
+                               Layout, Figure, Data)
+import numpy as np
 
-class Testgraph(TemplateView):
+class Testgraph1(TemplateView):
     template_name = 'graph.html'
 
     def get_context_data(self, **kwargs):
-        context = super(Testgraph, self).get_context_data(**kwargs)
+        context = super(Testgraph1, self).get_context_data(**kwargs)
 
         x = [-2,0,4,6,7]
         y = [q**2-q+3 for q in x]
-        trace1 = go.Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
-                            mode="lines",  name='1st Trace')
+        trace1 = Scatter(x=x, y=y, marker={'color': 'red', 'symbol': 104, 'size': "10"},
+                         mode="lines",  name='1st Trace')
 
-        data=go.Data([trace1])
-        layout=go.Layout(title="some diagram", xaxis={'title':'x1'}, yaxis={'title':'x2'})
-        figure=go.Figure(data=data,layout=layout)
-        div = opy.plot(figure, auto_open=False, output_type='div')
+        data=Data([trace1])
+        layout=Layout(title="some diagram", xaxis={'title':'x1'}, yaxis={'title':'x2'})
+        figure=Figure(data=data,layout=layout)
+        div = plot(figure, auto_open=False, output_type='div', show_link=False)
 
         return div
+
+class Testgraph2(TemplateView):
+    
+    def get_context_data(self, **kwargs):
+        x = np.random.randn(2000)
+        y = np.random.randn(2000)
+        div = plot([Histogram2dContour(x=x, y=y, contours=Contours(coloring='heatmap')),
+                    Scatter(x=x, y=y, mode='markers', marker=Marker(color='white', size=3, opacity=0.3))],
+                   show_link=False, output_type='div') 
+        return div
+    
 
 def index(request):
     template = loader.get_template('study_area/index.html')
     context = {}
-    g = Testgraph()
-    context['graph'] = g.get_context_data()
+    context['graph1'] = Testgraph1().get_context_data()
+    context['graph2'] = Testgraph2().get_context_data()
     html = template.render(context, request)
     return HttpResponse(html)    
