8b747e38c58b451adab9bd62fa322b0931bf61be
Stefaan Hessmann <hessmann.stefaan@googlemail.com>
2017-09-22 12:51:04 +0200

    pytest

--- repair/tests/test_project.py
+++ repair/tests/test_project.py
@@ -3,5 +3,5 @@ from django.conf import settings
 def pytest_configure():
     settings.configure(DJANGO_SETTINGS_MODULE = test_settings)
 
-def test_{{ project_name }}():
+def test_dummy():
     assert True
