e566717fb5161fbd3cfec9c3589101d17b99fc0d
ChrFr <christoph.franke@web.de>
2017-10-10 18:45:16 +0200

    Update: buttons, activity-group-tab for admin area

--- repair/static/js/admin-area.js
+++ repair/static/js/admin-area.js
@@ -1,3 +1,26 @@
 require(['./libs/domReady!', './app'], function (doc, app) {
+
+    var deactivateTabs = function(){
+        var tabs = document.querySelectorAll('.admin-tab');
+        console.log(tabs);
+        tabs.forEach(function(tab) {
+            tab.classList.remove('active');
+        });
+    }
+    
+    document.getElementById('enter-data-btn').addEventListener(
+        'click', function(){
+            deactivateTabs();
+            var tab =  document.getElementById('activity-groups-edit');
+            tab.classList.add('active');
+        });
+
+    document.getElementById('view-data-btn').addEventListener(
+        'click', function(){
+            deactivateTabs();
+            var tab =  document.getElementById('sankey');
+            tab.classList.add('active');
+        });
+
     requirejs(['app/admin-data-tree']);
 });
