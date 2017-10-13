22bcd89376be350f3cf7d9e56622aabc95b85c86
Tom Commandeur <t.j.f.commandeur@tudelft.nl>
2017-10-13 15:29:32 +0200

    changed graph labels

--- repair/static/js/app/visualizations/mapviewer.js
+++ repair/static/js/app/visualizations/mapviewer.js
@@ -5,8 +5,6 @@ define([
 ], function()
 {
   var Map = function(options){
-    var _this = this;
-
     var map = L.map(options.divid, {
         crs: L.CRS.EPSG4326,
         center: new L.LatLng(50,15),
@@ -17,9 +15,6 @@ define([
     $.each(options.baseLayers, function(name, layer) {
       layer.addTo(map);
     });
-    //$.each(options.overlayLayers, function(name, layer) {
-      //layer.addTo(map);
-    //});
     
     L.control.layers(options.baseLayers, options.overlayLayers).addTo(map);
   };
