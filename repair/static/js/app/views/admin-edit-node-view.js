6be490504560dee40fa8d5348f24b6d4a5757e91
ChrFr <christoph.franke@web.de>
2017-10-26 14:18:27 +0200

    Update: admin-area - functionality for adding rows to input/output/stock

--- repair/static/js/app/views/admin-edit-node-view.js
+++ repair/static/js/app/views/admin-edit-node-view.js
@@ -9,13 +9,19 @@ define(['backbone', 'app/models/activitygroup', 'app/models/activity',
         this.material = options.material;
         this.render();
       },
+      
+      events: {
+        'click #add-input-button, #add-stock-button, #add-output-button': 'addRowEvent'
+      },
 
       render: function(){
         var html = document.getElementById(this.template).innerHTML
         var template = _.template(html);
+        if (document.getElementById('input-table') != null)
+          console.log(document.getElementById('input-table').innerHTML)
         this.el.innerHTML = template();
         
-        var attrDiv = document.getElementById('attributes');
+        var attrDiv = this.el.querySelector('#attributes');
         var inner = '';
         if (this.model.tag == 'activity') 
           inner = this.getActivityAttrTable();
@@ -23,8 +29,37 @@ define(['backbone', 'app/models/activitygroup', 'app/models/activity',
           inner = this.getGroupAttrTable();
         else if (this.model.tag == 'actor') 
           inner = this.getActorAttrTable();
-        console.log(inner)
         attrDiv.innerHTML = inner;
+      },
+      
+      addRowEvent: function(event){
+        var buttonId = event.currentTarget.id;
+        var rowTemplateId;
+        var tableId = (buttonId == 'add-input-button') ? 'input-table': 
+                      (buttonId == 'add-output-button') ? 'output-table':
+                      'stock-table'
+        // common to all tables
+        var templateOptions = {
+            amount: 0,
+            qualities: [1, 2, 3, 4],
+            datasource: ''
+        };
+        // stock has no origin or destination
+        if (buttonId == 'add-input-button' || buttonId == 'add-output-button'){
+          templateOptions.nodes = this.model.collection;
+          rowTemplateId = 'input-output-row';
+        }
+        else
+          rowTemplateId = 'stock-row';
+        
+        this.addTableRow(tableId, rowTemplateId, templateOptions)
+      },
+      
+      addTableRow: function(tableId, rowTemplateId, options){
+          var el = this.el.querySelector('#' + tableId);
+          var rowHTML = document.getElementById(rowTemplateId).innerHTML;
+          var rowTemplate = _.template(rowHTML);
+          el.innerHTML += rowTemplate(options);
       },
       
       getGroupAttrTable: function(){
@@ -64,6 +99,7 @@ define(['backbone', 'app/models/activitygroup', 'app/models/activity',
       },
 
       close: function(){
+        this.undelegateEvents();
         this.unbind(); // Unbind all local event bindings
         //this.remove(); // Remove view from DOM
         this.el.innerHTML = ''; //empty the DOM element
