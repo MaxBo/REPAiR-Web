0799b872fd078c3c0a01e5a7769eccf2cfc06fa4
MaxBo <max.bohnet@tuhh.de>
2017-10-27 20:55:13 +0200

    add Nodes and Linkes to studyarea

--- repair/apps/studyarea/models.py
+++ repair/apps/studyarea/models.py
@@ -16,3 +16,23 @@ class Stakeholder(GDSEUniqueNameModel):
     @property
     def casestudy(self):
         return self.stakeholder_category.casestudy
+
+
+class Nodes(models.Model):
+    #node_id = models.IntegerField(primary_key=True)
+    location = models.CharField(max_length=255)
+    x_coord = models.FloatField()
+    y_coord = models.FloatField()
+
+
+class Links(models.Model):
+    #link_id = models.IntegerField(primary_key=True)
+    id_from = models.CharField(max_length=255)
+    id_to = models.CharField(max_length=255)
+    #link_description = models.CharField(max_length=100)
+    weight = models.FloatField()
+
+
+class Person(models.Model):
+    first_name = models.CharField(max_length = 255)
+    last_name = models.CharField(max_length = 255)
