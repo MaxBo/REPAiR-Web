1d2bae21025efbed068ef1f9eea8a51e63d0deb9
Stefaan <hessmann.stefaan@googlemail.com>
2017-11-09 17:29:48 +0100

    general model test

--- repair/tests/test.py
+++ repair/tests/test.py
@@ -3,19 +3,21 @@ from django.core.validators import ValidationError
 
 from repair.apps.login.models import CaseStudy, User, Profile
 from repair.apps.login.factories import *
+from repair.apps.asmfa.factories import *
 from rest_framework.test import APIRequestFactory
 from rest_framework.test import APITestCase
 from django.urls import reverse
 from rest_framework import status
+import json
+from unittest import skipIf
 
-class ModelTest(TestCase):
+class ModelTest(object):
     url_key = ""
     url_pks = dict()
     url_pk = dict()
     post_data = dict()
     put_data = dict()
     patch_data = dict()
-    factory = None
 
     def test_list(self):
         url = reverse(self.url_key + '-list', kwargs=self.url_pks)
@@ -23,12 +25,11 @@ class ModelTest(TestCase):
         assert response.status_code == 200
 
     def test_detail(self):
-        cs = self.factory()
         url = reverse(self.url_key + '-detail', kwargs={ **self.url_pks,
-                                                   'pk': cs.pk,})
+                                                         'pk': self.fact.pk,})
         # test get
         response = self.client.get(url)
-        assert response.data['id'] == cs.pk
+        assert response.data['id'] == self.fact.pk
         # check status code for put
         response = self.client.put(url, data=self.put_data, format='json')
         assert response.status_code == 200
@@ -42,12 +43,11 @@ class ModelTest(TestCase):
         # check if name has changed
         response = self.client.get(url)
         for key in self.patch_data:
-            assert response.data[key] == patch_data[key]
+            assert response.data[key] == self.patch_data[key]
 
     def test_delete(self):
-        cs = self.factory()
         url = reverse(self.url_key + '-detail', kwargs={**self.url_pks,
-                                                        'pk': cs.pk, })
+                                                        'pk': self.fact.pk, })
         response = self.client.get(url)
         assert response.status_code == 200
         response = self.client.delete(url)
@@ -58,8 +58,8 @@ class ModelTest(TestCase):
         url = reverse(self.url_key +'-list', kwargs=self.url_pks)
         # post
         response = self.client.post(url, self.post_data)
-        for key in post_data:
-            assert response.data[key] == data[key]
+        for key in self.post_data:
+            assert response.data[key] == self.post_data[key]
         # get
         new_id = response.data['id']
         url = reverse(self.url_key + '-detail', kwargs={**self.url_pks,
