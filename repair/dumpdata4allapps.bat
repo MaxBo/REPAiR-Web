a2d034da266fc2f8776c1a6e799e3a6a2d48f72a
MaxBo <max.bohnet@tuhh.de>
2017-11-16 20:36:53 +0100

    add fixtures and batch script to dump all fixtures from all apps

---
+++ repair/dumpdata4allapps.bat
@@ -0,0 +1,6 @@
+python manage.py dumpdata auth.group auth.user auth.user_groups --indent 2 --output %~dp0apps\login\fixtures\auth_fixture.json
+python manage.py dumpdata login --indent 2 --output %~dp0apps\login\fixtures\login_fixture.json
+python manage.py dumpdata login --indent 2 --output %~dp0apps\login\fixtures\login_fixture.json
+python manage.py dumpdata asmfa --indent 2 --output %~dp0apps\asmfa\fixtures\activities_dummy_data.json
+python manage.py dumpdata studyarea --indent 2 --output %~dp0apps\studyarea\fixtures\stakeholder_fixture.json
+python manage.py dumpdata changes --indent 2 --output %~dp0apps\changes\fixtures\changes_fixture.json
