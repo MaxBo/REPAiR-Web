8d5d389cbef088128d1081462b2a2e07226aeef5
Stefaan <hessmann.stefaan@googlemail.com>
2017-12-08 18:09:11 +0100

    solution tests

--- repair/apps/login/tests/test_login.py
+++ repair/apps/login/tests/test_login.py
@@ -148,6 +148,85 @@ class SolutioncategoryInCasestudyTest(BasicModelTest, APITestCase):
                                            )
 
 
+class SolutionInSolutioncategoryInCasestudyTest(BasicModelTest, APITestCase):
+
+    casestudy = 17
+    solutioncategory = 21
+    solution = 36
+    userincasestudy = 67
+    user = 99
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        #cls.cs_url = cls.baseurl + reverse('casestudy-detail',
+                                       #kwargs=dict(pk=cls.casestudy))
+        cls.solutioncategory_url = cls.baseurl + \
+            reverse('solutioncategory-detail',
+                    kwargs=dict(pk=cls.solutioncategory,
+                                casestudy_pk=cls.casestudy))
+        cls.url_key = "solution"
+        cls.url_pks = dict(casestudy_pk=cls.casestudy,
+                           solutioncategory_pk=cls.solutioncategory)
+        cls.url_pk = dict(pk=cls.solution)
+        cls.post_data = dict(name='posttestname',
+                             user='http://testserver' + \
+                             reverse('userincasestudy-detail',
+                                     kwargs=dict(pk=cls.uic.id,
+                                                 casestudy_pk=cls.casestudy)),
+                             description="This is a description",
+                             one_unit_equals='20',
+                             solution_category=cls.solutioncategory_url,
+                             )
+        cls.put_data = cls.post_data
+        cls.patch_data = dict(name="test name")
+
+    def setUp(self):
+        self.obj = SolutionFactory(id=self.solution,
+                                   solution_category__id=self.solutioncategory,
+                                   solution_category__user=self.uic,
+                                   user=self.uic,
+                                   )
+
+
+class SolutionquantityInSolutionInSolutioncategoryInCasestudyTest(BasicModelTest, APITestCase):
+
+    casestudy = 17
+    solutioncategory = 21
+    solution = 36
+    solutionquantity = 28
+    userincasestudy = 67
+    user = 99
+    unit = 75
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.unit_url = cls.baseurl + reverse('unit-detail',
+                                             kwargs=dict(pk=cls.unit))
+        #cls.solutioncategory_url = cls.baseurl + \
+            #reverse('solutioncategory-detail',
+                    #kwargs=dict(pk=cls.solutioncategory,
+                                #casestudy_pk=cls.casestudy))
+        cls.url_key = "solutionquantity"
+        cls.url_pks = dict(casestudy_pk=cls.casestudy,
+                           solutioncategory_pk=cls.solutioncategory,
+                           solution_pk=cls.solution)
+        cls.url_pk = dict(pk=cls.solutionquantity)
+        cls.post_data = dict(name='posttestname',
+                             unit=cls.unit_url,
+                             )
+        cls.put_data = cls.post_data
+        cls.patch_data = dict(name="test name")
+
+    def setUp(self):
+        self.obj = SolutionQuantityFactory(id=self.solutionquantity,
+                                           solution__id=self.solution,
+                                           unit__id=self.unit,
+                                           solution__solution_category__id=self.solutioncategory,
+                                           solution__solution_category__user=self.uic,
+                                           solution__user=self.uic,
+                                           )
+
+
 class UserInCasestudyTest(BasicModelTest, APITestCase):
 
     casestudy = 17
