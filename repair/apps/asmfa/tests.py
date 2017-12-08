7f7e9fd8d62858fa2681c8fe78f93d57e6ac7beb
Stefaan <hessmann.stefaan@googlemail.com>
2017-12-08 16:52:32 +0100

    product test

--- repair/apps/asmfa/tests.py
+++ repair/apps/asmfa/tests.py
@@ -506,6 +506,7 @@ class ActorstockInKeyflowInCasestudyTest(BasicModelTest, APITestCase):
         """
         pass
 
+
 class GroupstockInKeyflowInCasestudyTest(BasicModelTest, APITestCase):
     """
     MAX:
@@ -555,6 +556,37 @@ class GroupstockInKeyflowInCasestudyTest(BasicModelTest, APITestCase):
         not working: produvt id required
         """
         pass
+
+
+class ProductsInKeyflowInCasestudyTest(BasicModelTest, APITestCase):
+    """
+    MAX:
+        1. Products is not in casestudy/xx/keyflows/xx/
+        2. not shure what fractions is
+    """
+    casestudy = 17
+    keyflow = 3
+    product = 16
+    keyflowincasestudy = 45
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
+        cls.url_key = "product"
+        cls.url_pks = dict(casestudy_pk=cls.casestudy,
+                           keyflow_pk=cls.keyflowincasestudy)
+        cls.url_pk = dict(pk=cls.product)
+        cls.put_data = dict(fractions=[])
+        cls.post_data = cls.put_data
+        cls.patch_data = dict(name="other name")
+        cls.sub_urls = ['keyflow']
+
+    def setUp(self):
+        kic_obj = KeyflowInCasestudyFactory(id=self.keyflowincasestudy,
+                                            casestudy=self.uic.casestudy,
+                                            keyflow__id=self.keyflow)
+        self.obj = ProductFactory(id=self.product,
+                                  keyflow=kic_obj,
+                                  )
 
 
 class GeolocationViewTest(APITestCase):
