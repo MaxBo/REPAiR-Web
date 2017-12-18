8db1fe8c35ddd12f71b0176758bc159c2a0c873b
MaxBo <max.bohnet@tuhh.de>
2017-12-18 21:23:54 +0100

    areas added

--- repair/apps/studyarea/models.py
+++ repair/apps/studyarea/models.py
@@ -1,4 +1,6 @@
 from django.db import models
+from django.contrib.gis.db import models as geomodels
+
 from repair.apps.login.models import GDSEUniqueNameModel, CaseStudy
 
 
@@ -14,6 +16,89 @@ class Stakeholder(GDSEUniqueNameModel):
     @property
     def casestudy(self):
         return self.stakeholder_category.casestudy
+
+
+class Area(GDSEUniqueNameModel):
+    name = models.TextField()
+    geom = geomodels.MultiPolygonField(null=True, blank=True)
+
+
+class World(Area):
+    """TopLevel"""
+    level = 1
+
+
+class Continent(Area):
+    """TopLevel"""
+    level = 2
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='continents')
+
+
+class Country(Area):
+    """TopLevel"""
+    level = 3
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='countries')
+
+
+class NUTS1(Area):
+    """TopLevel"""
+    level = 4
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='nuts1_areas')
+
+
+class NUTS2(Area):
+    """TopLevel"""
+    level = 5
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='nuts2_areas')
+
+
+class NUTS3(Area):
+    """TopLevel"""
+    level = 6
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='nuts3_areas')
+
+
+class LAU1(Area):
+    """TopLevel"""
+    level = 7
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='lau1_areas')
+
+
+class LAU2(Area):
+    """TopLevel"""
+    level = 8
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='lau2_areas')
+
+
+class CityDistrict(Area):
+    """TopLevel"""
+    level = 9
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='citydistricts')
+
+
+class CityNeighbourhood(Area):
+    """TopLevel"""
+    level = 10
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='neighbourhoods')
+
+
+class CityBlock(Area):
+    """TopLevel"""
+    level = 11
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='blocks')
+
+
+class StreetSection(Area):
+    """TopLevel"""
+    level = 12
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='streetsections')
+
+
+class House(Area):
+    """TopLevel"""
+    level = 13
+    parent_area = models.ForeignKey("Area", null=True, blank=True, related_name='houses')
+
 
 
 class Nodes(models.Model):
--- repair/apps/studyarea/test_stakeholders.py
+++ repair/apps/studyarea/test_stakeholders.py
@@ -9,6 +9,7 @@ from repair.apps.studyarea.factories import (CaseStudyFactory,
                                              StakeholderCategoryFactory,
                                              StakeholderFactory,
                                              )
+import repair.apps.studyarea.models as models
 
 
 
@@ -103,8 +104,35 @@ class StakeholdercategoryInCasestudyTest(BasicModelTest, APITestCase):
                                       self.uic.casestudy,
                                       )
 
-    #def test_post(self):
-        #"""
-        #MAX
-        #"""
-        #pass
+
+
+class AreaModels(TestCase):
+    def test_01_dynamic_models(self):
+        world = models.World.objects.create(name='Earth')
+        eu = models.Continent.objects.create(name='EU')
+        spain = models.Country.objects.create(name='ES')
+        de = models.Country.objects.create(name='DE')
+        hh = models.NUTS1.objects.create(name='Hamburg')
+        catalunia = models.NUTS1.objects.create(name='Catalunia')
+
+        eu.parent_area = world
+        spain.parent_area = eu
+        de.parent_area = eu
+        hh.parent_area = de
+        catalunia.parent_area = eu
+
+        eu.save()
+        spain.save()
+        de.save()
+        hh.save()
+        catalunia.save()
+
+        areas = models.Area.objects.all()
+        assert areas.count() == 6
+
+        self.assertSetEqual(set(eu.countries.all()), {spain, de})
+        self.assertSetEqual(set(eu.nuts1_areas.all()), {catalunia})
+
+        self.assertEqual(models.Area.objects.get(name='ES').country, spain)
+
+        print(areas)
