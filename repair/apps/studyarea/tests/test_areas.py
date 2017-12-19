4313f3d0e301dff99f22ba11766921b3da86c552
MaxBo <max.bohnet@tuhh.de>
2017-12-19 09:42:51 +0100

    extend test

--- repair/apps/studyarea/tests/test_areas.py
+++ repair/apps/studyarea/tests/test_areas.py
@@ -10,24 +10,28 @@ class AreaModels(TestCase):
         de = models.Country.objects.create(name='DE')
         hh = models.NUTS1.objects.create(name='Hamburg')
         catalunia = models.NUTS1.objects.create(name='Catalunia')
+        castilia = models.NUTS1.objects.create(name='Castilia')
 
         eu.parent_area = world
         spain.parent_area = eu
         de.parent_area = eu
         hh.parent_area = de
+        castilia.parent_area = spain
         catalunia.parent_area = eu
 
         eu.save()
         spain.save()
         de.save()
         hh.save()
+        castilia.save()
         catalunia.save()
 
         areas = models.Area.objects.all()
-        assert areas.count() == 6
+        assert areas.count() == 7
 
         self.assertSetEqual(set(eu.countries.all()), {spain, de})
         self.assertSetEqual(set(eu.nuts1_areas.all()), {catalunia})
+        self.assertSetEqual(set(spain.nuts1_areas.all()), {castilia})
 
         self.assertEqual(models.Area.objects.get(name='ES').country, spain)
 
