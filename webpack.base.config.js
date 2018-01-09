9d092979e7a159cdf480b78c7f9c7fcef008c1b3
ChrFr <christoph.franke@web.de>
2018-01-09 14:15:22 +0100

    Update: split webpack-config

---
+++ webpack.base.config.js
@@ -0,0 +1,50 @@
+var path = require('path');
+var webpack = require('webpack');
+var BundleTracker = require('webpack-bundle-tracker');
+
+module.exports = {
+  context: __dirname,
+  entry: {
+    DataEntry: './repair/static/js/data-entry',
+    //vendors: './repair/static/bundles/local/vendors.js'
+  },
+  
+  output: {
+    path: path.resolve('./repair/static/bundles/local/'),
+    publicPath: '/static/bundles/local/',
+    filename: "[name]-[hash].js"
+  },
+
+  plugins: [
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendors', filename: 'vendors.js' }),
+  ],
+  
+  node: { fs: 'empty', net: 'empty', tls: 'empty', child_process: 'empty', __filename: true, __dirname: true }, 
+  
+  externals: [ 'ws' ],
+  //target: 'node',
+  
+  module: {
+    //exprContextRegExp: /^\.\/.*$/,
+    //unknownContextRegExp: /^\.\/.*$/,
+    rules: [] // add all common loaders here
+  },
+
+  resolve: {
+    modules : ['static/js', 'node_modules', 'bower_components'],
+      alias: {
+        "treeview": "libs/bootstrap-treeview.min",
+        "tablesorter-pager": "libs/jquery.tablesorter.pager",
+        'ol-contextmenu': 'libs/ol-contextmenu',
+        'spatialsankey': 'libs/spatialsankey'
+      },
+    plugins: [
+      new webpack.ProvidePlugin({
+        _: 'loadash',
+        d3: 'd3'
+      })
+    ]
+    //extensions: ['.js', '.jsx']
+  },
+}
+
