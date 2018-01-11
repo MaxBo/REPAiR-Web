2dda1702f70dd536e2a5580a39ccbb466960839d
ChrFr <christoph.franke@web.de>
2018-01-11 12:19:40 +0100

    Fixed: casestudies not selectable in all views

--- repair/apps/decisions/urls.py
+++ repair/apps/decisions/urls.py
@@ -3,5 +3,5 @@ from django.conf.urls import url
 from repair.apps.decisions import views
 
 urlpatterns = [
-    url(r'^$', views.index, name='decisions')
+    url(r'^$', views.DecisionsIndexView.as_view(), name='index'),
 ]
