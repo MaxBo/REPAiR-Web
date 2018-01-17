a59f4ea988d865a597ffad1644ee27f2e963273e
ChrFr <christoph.franke@web.de>
2018-01-17 11:49:14 +0100

    New Feature: settings for a staged server

---
+++ repair/settings_staged.py
@@ -0,0 +1,26 @@
+from repair.settings import *
+
+DEBUG = True
+
+# Database
+# https://docs.djangoproject.com/en/1.11/ref/settings/#databases
+
+DATABASES = {
+    'default': {
+        'ENGINE': 'django.contrib.gis.db.backends.spatialite',
+        'NAME': os.path.join(PROJECT_DIR, 'db.sqlite3'),
+        'OPTIONS': {
+             'timeout': 20,
+         }
+    },
+}
+
+SPATIALITE_LIBRARY_PATH = 'mod_spatialite'
+
+WEBPACK_LOADER = {
+    'DEFAULT': {
+        'CACHE': not DEBUG, 
+        'BUNDLE_DIR_NAME': 'bundles/staged/', 
+        'STATS_FILE': os.path.join(PROJECT_DIR, 'webpack-stats-staged.json'),
+    }
+}
