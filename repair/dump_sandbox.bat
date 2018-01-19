dcc484001b607096b6401cdb9cd80c6499511289
MaxBo <max.bohnet@tuhh.de>
2018-01-19 22:10:22 +0100

    dummy-data with areas, adminlevel

--- repair/dump_sandbox.bat
+++ repair/dump_sandbox.bat
@@ -1,14 +1,13 @@
 SET DJANGO_SETTINGS_MODULE=%DJANGO_SITENAME%.settings_staged
-REM python manage.py dump_object -k asmfa.actor --query "{\"activity__activitygroup__keyflow__casestudy__id\": 7}" > repair\fixtures\sandbox_actor.json
+python manage.py dump_object -k asmfa.actor --query "{\"activity__activitygroup__keyflow__casestudy__id\": 7}" > repair\fixtures\sandbox_actor.json
 
-REM python manage.py dump_object -k asmfa.keyflowincasestudy --query "{\"casestudy__id\": 7}" > repair\fixtures\sandbox_keyflow.json
-REM python manage.py dump_object -k asmfa.productfraction --query "{\"product__keyflow__casestudy__id\": 7}"  > repair\fixtures\sandbox_products.json
-REM python manage.py dump_object -k studyarea.area --query "{\"casestudy__id\": 7}"  > repair\fixtures\sandbox_areas.json
-REM python manage.py dump_object -k changes.solutioninimplementation --query "{\"implementation__user__casestudy__id\": 5}"  > repair\fixtures\sandbox_solutions.json
-REM python manage.py dump_object -k changes.solutionratiooneunit --query "{\"solution__user__casestudy__id\": 5}"  > repair\fixtures\sandbox_solutionsratio.json
-REM python manage.py dump_object -k changes.strategy --query "{\"user__casestudy__id\": 5}"  > repair\fixtures\sandbox_strategy.json
-REM python manage.py dump_object -k publications_bootstrap.publication * > repair\fixtures\sandbox_publications.json
-REM python manage.py dump_object -k studyarea.area --query "{\"casestudy__id\": 7}"  > repair\fixtures\sandbox_areas.json
+python manage.py dump_object -k asmfa.keyflowincasestudy --query "{\"casestudy__id\": 7}" > repair\fixtures\sandbox_keyflow.json
+python manage.py dump_object -k asmfa.productfraction --query "{\"product__keyflow__casestudy__id\": 7}"  > repair\fixtures\sandbox_products.json
+python manage.py dump_object -k changes.solutioninimplementation --query "{\"implementation__user__casestudy__id\": 5}"  > repair\fixtures\sandbox_solutions.json
+python manage.py dump_object -k changes.solutionratiooneunit --query "{\"solution__user__casestudy__id\": 5}"  > repair\fixtures\sandbox_solutionsratio.json
+python manage.py dump_object -k changes.strategy --query "{\"user__casestudy__id\": 5}"  > repair\fixtures\sandbox_strategy.json
+python manage.py dump_object -k publications_bootstrap.publication * > repair\fixtures\sandbox_publications.json
+python manage.py dump_object -k studyarea.area --query "{\"adminlevel__casestudy__id\": 7}"  > repair\fixtures\sandbox_areas.json
 python manage.py merge_fixtures^
  repair\fixtures\sandbox_casestudy.json^
  repair\fixtures\sandbox_areas.json^
