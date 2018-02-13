ac6fe4051774fda8d41608b8d0b173e27eebcf15
Stefaan <hessmann.stefaan@googlemail.com>
2018-02-13 15:59:25 +0100

    fix typo

--- repair/tests/EditActorTest_orig.py
+++ repair/tests/EditActorTest_orig.py
@@ -21,9 +21,8 @@ class EditActorTest(SeleniumBasic, unittest.TestCase):
         driver = self.driver
         # login
         driver.find_element_by_css_selector("#login-link > div.dropdown > button.dropdown-button").click()
-        driver.find_element_by_id("data-entry-lin"
-                                  "k").click()
-        driver.find_element_by_link_text("Edit Actosdrs").click()
+        driver.find_element_by_id("data-entry-link").click()
+        driver.find_element_by_link_text("Edit Actors").click()
         driver.find_element_by_id("keyflow-select").click()
         Select(driver.find_element_by_id("keyflow-select")).select_by_visible_text("Organic")
         driver.find_element_by_id("keyflow-select").click()
