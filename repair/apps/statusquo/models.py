fc0b12c4ca7bc82841e00ef934c3825f268b620d
Stefaan <hessmann.stefaan@googlemail.com>
2018-02-16 19:00:07 +0100

    models and serializers for aims challenges and targets

--- repair/apps/statusquo/models.py
+++ repair/apps/statusquo/models.py
@@ -1,3 +1,56 @@
-from django.db import models
+# -*- coding: utf-8 -*-
 
-# Create your models here.
+from django.db import models
+#from __future__ import unicode_literals
+
+
+from repair.apps.login.models import (CaseStudy, User, GDSEModel)
+
+
+
+class Aim(GDSEModel):
+    casestudy = models.ForeignKey(CaseStudy, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
+
+class Challenge(GDSEModel):
+    casestudy = models.ForeignKey(CaseStudy, on_delete=models.CASCADE)
+    text = models.CharField(max_length=255)
+
+
+class IndicatorSustainabilityField(GDSEModel):
+    name = models.CharField(max_length=255)
+
+
+class IndicatorAreaOfProtection(GDSEModel):
+    name = models.CharField(max_length=255)
+    sustainability_field = models.ForeignKey(IndicatorSustainabilityField,
+                                             on_delete=models.CASCADE)
+
+class IndicatorImpactCategory(GDSEModel):
+    name = models.CharField(max_length=255)
+    area_of_protection = models.ForeignKey(IndicatorAreaOfProtection,
+                                           on_delete=models.CASCADE)
+    spatial_differentiation = models.BooleanField()
+
+
+class TargetSpatialReference(GDSEModel):
+    name = models.CharField(max_length=255)
+    text = models.CharField(max_length=255)
+
+
+class TargetValue(GDSEModel):
+    text = models.CharField(max_length=255)
+    number = models.FloatField()
+    factor = models.FloatField()
+
+
+class Target(GDSEModel):
+    casestudy = models.ForeignKey(CaseStudy, on_delete=models.CASCADE)
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    aim = models.ForeignKey(Aim, on_delete=models.CASCADE)
+    impact_category = models.ForeignKey(IndicatorImpactCategory,
+                                        on_delete=models.CASCADE)
+    target_value = models.ForeignKey(TargetValue, on_delete=models.CASCADE)
+    spatial_reference = models.ForeignKey(TargetSpatialReference,
+                                          on_delete=models.CASCADE)
--- repair/apps/statusquo/serializers.py
+++ repair/apps/statusquo/serializers.py
@@ -1,1 +1,121 @@
 from rest_framework import serializers
+from django.utils.translation import ugettext_lazy as _
+from repair.apps.login.models import CaseStudy, User
+from repair.apps.login.serializers import (NestedHyperlinkedModelSerializer,
+                                           InCasestudySerializerMixin,
+                                           InCasestudyField,
+                                           InCasestudyListField,
+                                           IdentityFieldMixin,
+                                           NestedHyperlinkedRelatedField,
+                                           IDRelatedField)
+from repair.apps.statusquo.models import (Aim,
+                                          Challenge,
+                                          IndicatorAreaOfProtection,
+                                          IndicatorImpactCategory,
+                                          IndicatorSustainabilityField,
+                                          Target,
+                                          TargetSpatialReference,
+                                          TargetValue)
+
+
+class AimSerializer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {}
+    casestudy = IDRelatedField()
+    text = serializers.CharField()
+
+    class Meta:
+        model = Aim
+        fields = ('url',
+                  'id',
+                  'casestudy',
+                  'text')
+
+
+class ChallengeSerializer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {}
+    casestudy = IDRelatedField()
+    text = serializers.CharField()
+
+    class Meta:
+        model = Challenge
+        fields = ('url',
+                  'id',
+                  'casestudy',
+                  'text')
+
+class IndicatorAreaOfProtectionSerializer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {}
+    name = serializers.CharField()
+    sustainability_field = IDRelatedField()
+
+    class Meta:
+        model = IndicatorAreaOfProtection
+        fields = ('url',
+                  'id',
+                  'name',
+                  'sustainability_field')
+
+
+class IndicatorImpactCategorySerielizer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {}
+    name = serializers.CharField()
+    area_of_protection = IDRelatedField()
+    spatial_differentiation = serializers.BooleanField()
+
+    class Meta:
+        model = IndicatorImpactCategory
+        fields = ('url',
+                  'id',
+                  'name',
+                  'area_of_protection',
+                  'spatial_differentiation')
+
+
+class IndicatorSustainabilityFieldSerializer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {}
+    name = serializers.CharField()
+
+    class Meta:
+        model = IndicatorSustainabilityField
+        fields = ('url',
+                  'id',
+                  'name')
+
+
+class TargetSerializer(NestedHyperlinkedModelSerializer):
+    parent_lookup_kwargs = {}
+    casestudy = IDRelatedField()
+    user = IDRelatedField()
+    aim = IDRelatedField()
+    impact_category = IDRelatedField()
+    target_value = IDRelatedField()
+    spatial_reference = IDRelatedField()
+
+    class Meta:
+        model = Target
+        fields = ('url',
+                  'id',
+                  'casestudy',
+                  'user',
+                  'aim',
+                  'impact_category',
+                  'target_value',
+                  'spatial_reference')
+
+
+class TargetValueSerializer(NestedHyperlinkedModelSerializer):
+    text = serializers.CharField()
+    number = serializers.FloatField()
+    factor = serializers.FloatField()
+
+    class Meta:
+        model = TargetValue
+        fields = ('url',
+                  'id',
+                  'text',
+                  'number',
+                  'factor')
+
+
+
+
