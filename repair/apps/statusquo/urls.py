acb13709edadacd4200712fe2853cb7d02f90e62
ChrFr <christoph.franke@web.de>
2018-03-14 15:02:06 +0100

    Refactored: status quo - merged workshop and setup entry points

--- repair/apps/statusquo/urls.py
+++ repair/apps/statusquo/urls.py
@@ -3,7 +3,5 @@ from django.conf.urls import url
 from repair.apps.statusquo import views
 
 urlpatterns = [
-    url(r'^$', views.other.StatusQuoView.as_view(), name='status-quo'),
-
-    #
+    url(r'^$', views.statusquo.StatusQuoView.as_view(), name='status-quo')
 ]
---
+++ repair/apps/statusquo/views/statusquo.py
@@ -0,0 +1,20 @@
+from django.http import HttpResponse
+from django.template import loader
+from rest_framework import viewsets
+from repair.views import ModeView
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import render
+import numpy as np
+from repair.apps.login.views import  CasestudyViewSetMixin
+from repair.apps.utils.views import (ModelPermissionViewSet,
+                                     ReadUpdatePermissionViewSet)
+
+
+class StatusQuoView(LoginRequiredMixin, ModeView):
+    def render_setup(self, request):
+        return render(request, 'statusquo/index.html',
+                      self.get_context_data())
+
+    def render_workshop(self, request):
+        return render(request, 'statusquo/index.html',
+                      self.get_context_data())
