19fee85eb86c973c2e27b00e7c4be54b63b3f697
Balázs Dukai <balazs.dukai@gmail.com>
2018-04-09 18:00:47 +0200

    WIP targets
    pagination doesnt work for TargetValues

--- repair/js/collections/targetvalues.js
+++ repair/js/collections/targetvalues.js
@@ -1,36 +1,40 @@
-define(["backbone", "app-config"],
+define(["backbone-pageable", "app-config"],
 
-    function(Backbone, config) {
+  function(PageableCollection, config) {
 
-        /**
-         * @author Balázs Dukai
-         * @name module:collections/TargetValues
-         * @augments Backbone.Collection
-         */
-        var TargetValues = Backbone.Collection.extend(
-            /** @lends module:collections/TargetValues.prototype */
-            {
-                /**
-                 * generates an url to the api resource list based on the ids given in constructor
-                 *
-                 * @returns {string} the url string
-                 */
-                url: function() {
-                    return config.api.targetvalues;
-                },
+   /**
+    * collection for fetching/putting products
+    *
+    * @param {Array.<Object>} [attrs=null]   list objects representing the fields of each model and their values, will be set if passed
+    *
+    * @author Balázs Dukai
+    * @name module:collections/TargetValues
+    * @augments Backbone.PageableCollection
+    *
+    * @constructor
+    * @see https://github.com/backbone-paginator/backbone.paginator
+    */
+    var TargetValues = PageableCollection.extend(
+      /** @lends module:collections/TargetValues.prototype */
+      {
+      /**
+       * generates an url to the api resource
+       *
+       * @returns {string} the url string
+       */
+      url: function(){
+        return config.api.targetvalues;
+      },
 
-                /**
-                 * collection for fetching/putting categories of stakeholder definitions
-                 *
-                 * @param {Array.<Object>} [attrs=null]   list objects representing the fields of each model and their values, will be set if passed
-                 * @param {Object} options
-                 *
-                 * @constructs
-                 * @see http://backbonejs.org/#Collection
-                 */
-                initialize: function(attrs, options) {
-                }
-            })
-        return TargetValues;
-    }
+      queryParams: {
+        pageSize: "page_size"
+      },
+
+      parseRecords: function (response) {
+        return response.results;
+      }
+    });
+
+    return TargetValues;
+  }
 );
--- repair/js/views/status-quo/targets.js
+++ repair/js/views/status-quo/targets.js
@@ -60,25 +60,34 @@ function(_, BaseView, Aim, Aims, TargetValues){
                 }
             });
 
-            this.targetsModel.fetch({
-                success: function(targets){
-                    targets.forEach(function(t){
-                        var targets_array = t.attributes.results;
-                        targets_array.forEach(function(target){
-                            _this.targets.push({
-                                "text": target.text,
-                                "id": target.id,
-                                "number": target.number,
-                                "factor": target.factor
-                            });
-                        });
-                    });
-                    _this.render();
-                },
-                error: function(){
-                    console.error("cannot fetch targetvalues");
-                }
-            });
+
+            console.log(this.targetsModel.hasNextPage());
+            this.targetsModel.getFirstPage();
+            console.log(this.targetsModel.hasNextPage());
+            console.log(this.targetsModel);
+            this.targetsModel.getNextPage();
+            console.log(this.targetsModel);
+
+            // this.targetsModel.fetch({
+            //     success: function(targets){
+            //         console.log(targets);
+            //         // for each page?
+            //         targets.forEach(function(target){
+            //             // for each object
+            //             console.log(target);
+            //             _this.targets.push({
+            //                 "text": target.get('text'),
+            //                 "id": target.get('id'),
+            //                 "number": target.get('number'),
+            //                 "factor": target.get('factor')
+            //             });
+            //         });
+            //         _this.render();
+            //     },
+            //     error: function(){
+            //         console.error("cannot fetch targetvalues");
+            //     }
+            // });
 
             this.render();
         },
