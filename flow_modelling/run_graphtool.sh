a941beb9f9487c093a4ef83ef08d3062b7adfce2
Balázs Dukai <balazs.dukai@gmail.com>
2018-07-04 15:18:06 +0200

    Implement simple mass balance check

--- flow_modelling/run_graphtool.sh
+++ flow_modelling/run_graphtool.sh
@@ -2,14 +2,6 @@
 
 docker run --name gt_test -v "$(pwd)":/tmp -p 8888:8888 -p 6006:6006 -it -u root -w /home/user tiagopeixoto/graph-tool bash
 
-# install pip
-
-pacman -S python-pip
-
-# install jupyter
-
-pip install jupyter
-
 # start the notebook
 
 jupyter notebook --ip=0.0.0.0 --allow-root --notebook-dir=/tmp
