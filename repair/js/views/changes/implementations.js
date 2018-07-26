6360e10ca04e1235ee084cc753c01c4f7079d43c
ChrFr <christoph.franke@web.de>
2018-07-26 11:41:00 +0200

    map height

--- repair/js/views/changes/implementations.js
+++ repair/js/views/changes/implementations.js
@@ -532,8 +532,8 @@ var ImplementationsView = BaseView.extend(
         var _this = this,
             el = document.getElementById(divid);
         // calculate (min) height
-        var minHeight = document.body.clientHeight * 0.4;
-        el.style.minHeight = minHeight;
+        var minHeight = document.body.clientHeight;
+        el.style.minHeight = minHeight - 50 + 'px';
         // remove old map
         if (this.editorMap){
             this.editorMap.map.setTarget(null);
