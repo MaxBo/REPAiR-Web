bf091422a437a84345851158c30bd7e10d21fe0a
ChrFr <christoph.franke@web.de>
2018-09-18 12:51:06 +0200

    adapted targets view, removed auxiliary arrays, now collections only

--- repair/js/views/targets/sustainability-targets.js
+++ repair/js/views/targets/sustainability-targets.js
@@ -55,10 +55,19 @@ function(_, BaseView, GDSECollection, GDSEModel){
 
             var promises = [];
 
-            promises.push(this.targets.fetch({
+            promises.push(this.impactcategories.fetch({
                 error: _this.onError
             }));
-            promises.push(this.aims.fetch({
+
+            promises.push(this.targetvalues.fetch({
+                error: _this.onError
+            }));
+
+            promises.push(this.spatial.fetch({
+                error: _this.onError
+            }));
+
+            this.aims.fetch({
                 data: {
                     keyflow: this.keyflowId
                 },
@@ -68,24 +77,13 @@ function(_, BaseView, GDSECollection, GDSEModel){
                             apiTag: 'targets',
                             apiIds: [_this.caseStudy.id, aim.id]
                         });
+                        promises.push(_this.targets[aim.id].fetch({error: _this.onError}));
+                        Promise.all(promises).then(_this.render);
                     })
                 },
                 error: _this.onError
-            }));
+            });
 
-            promises.push(this.impactCategories.fetch({
-                error: _this.onError
-            }));
-
-            promises.push(this.targetValues.fetch({
-                error: _this.onError
-            }));
-
-            promises.push(this.spatial.fetch({
-                error: _this.onError
-            }));
-
-            Promise.all(promises).then(this.render);
         },
 
         /*
@@ -106,39 +104,27 @@ function(_, BaseView, GDSECollection, GDSEModel){
             this.renderRows();
         },
 
-        createSelect(type, typeObject, typeList, target){
+        createSelect(type, typeObject, typeList, target, aimId){
             var _this = this;
             var select = document.createElement('select'),
                 wrapper = document.createElement('div');
             // var type = "targetvalue";
-            var selectId = type + typeObject.id;
             wrapper.classList.add('fake-cell');
             select.classList.add('form-control');
             typeList.forEach(function(target){
                 var option = document.createElement('option');
                 if (type == "impact") {
-                    option.text = target.name;
+                    option.text = target.get('name');
                 } else {
-                    option.text = target.text;
+                    option.text = target.get('text');
                 }
-                option.setAttribute("id", target.id);
+                option.value = target.id;
                 select.appendChild(option);
             });
-            for(var k, j = 0; k = select.options[j]; j++) {
-                if (type == "impact") {
-                    if(k.text == typeObject.name) {
-                        select.selectedIndex = j;
-                        break;
-                    }
-                } else {
-                    if(k.text == typeObject.text) {
-                        select.selectedIndex = j;
-                        break;
-                    }
-                }
-            }
+            select.value = typeObject.id;
+
             select.setAttribute("type", type);
-            select.setAttribute("id", selectId);
+            select.setAttribute("aimId", aimId);
             select.setAttribute("targetId", target.id);
             select.addEventListener("change", function(e){
                 _this.editTarget(e, target);
@@ -154,45 +140,33 @@ function(_, BaseView, GDSECollection, GDSEModel){
                 row.classList.add('row', 'overflow', 'bordered');
                 var html = document.getElementById('sustainability-target-row-template').innerHTML
                 var template = _.template(html);
-                row.innerHTML = template({ aim: aim.text, aimId: aim.id });
-                row.setAttribute("rowAimId", aim.id);
+                row.innerHTML = template({ aim: aim.get('text'), aimId: aim.id });
                 _this.el.appendChild(row);
-            });
-            if (this.targets.length > 0) {
-                for (var i = 0; i < this.targets.length; i++){
-                    var target = this.targets[i];
-                    var aim = _this.getObject(_this.aims, target.aim),
-                        impactCategory = _this.getObject(_this.impactcategories,
-                            target.impact_category),
-                        targetValue = _this.getObject(_this.targetvalues,
-                            target.target_value),
-                        spatial = _this.getObject(_this.spatial,
-                            target.spatial_reference);
+                var indicatorPanel = row.querySelector('.indicators').querySelector('.item-panel'),
+                    targetPanel = row.querySelector('.targets').querySelector('.item-panel'),
+                    spatialPanel = row.querySelector('.spatial').querySelector('.item-panel'),
+                    removePanel = row.querySelector('.remove').querySelector('.item-panel'),
+                    html = document.getElementById('panel-item-template').innerHTML,
+                    template = _.template(html);
+                var targets = _this.targets[aim.id];
+                targets.forEach(function(target){
+                    var impactCategory = _this.impactcategories.get(target.get('impact_category')),
+                        targetValue = _this.targetvalues.get(target.get('target_value')),
+                        spatial = _this.spatial.get(target.get('spatial_reference'));
                     var removeBtn = document.createElement('button');
-                    if (i == 0 || this.targets[i-1].aim != target.aim) {
-                        var row = _this.el.querySelector("[rowaimid=" + CSS.escape(target.aim) + "]");
-                        var indicatorPanel = row.querySelector('.indicators').querySelector('.item-panel'),
-                            targetPanel = row.querySelector('.targets').querySelector('.item-panel'),
-                            spatialPanel = row.querySelector('.spatial').querySelector('.item-panel'),
-                            removePanel = row.querySelector('.remove').querySelector('.item-panel'),
-                            html = document.getElementById('panel-item-template').innerHTML,
-                            template = _.template(html);
-                    }
-
                     var targetSelect = _this.createSelect("targetvalue", targetValue,
-                     _this.targetvalues, target);
+                        _this.targetvalues, target, aim.id);
                     targetPanel.appendChild(targetSelect);
 
                     var panelItem = _this.createSelect("impact", impactCategory,
-                        _this.impactcategories, target);
+                        _this.impactcategories, target, aim.id);
                     indicatorPanel.appendChild(panelItem);
 
                     var spatialSelect = _this.createSelect("spatial", spatial,
-                    _this.spatial, target);
+                        _this.spatial, target, aim.id);
                     spatialPanel.appendChild(spatialSelect);
 
-                    removeBtn.classList.add("btn", "btn-warning", "square",
-                     "remove");
+                    removeBtn.classList.add("btn", "btn-warning", "square", "remove");
                     // removeBtn.style.float = 'right';
                     var span = document.createElement('span');
                     removeBtn.title = gettext('Remove target')
@@ -202,6 +176,7 @@ function(_, BaseView, GDSECollection, GDSEModel){
                     span.style.pointerEvents = 'none';
                     removeBtn.appendChild(span);
                     removeBtn.setAttribute("targetId", target.id);
+                    removeBtn.setAttribute("aimId", aim.id);
                     removeBtn.addEventListener('click', function(e){
                         _this.deleteTarget(e);
                     })
@@ -209,46 +184,29 @@ function(_, BaseView, GDSECollection, GDSEModel){
                     btnDiv.classList.add("row", "fake-cell");
                     btnDiv.appendChild(removeBtn);
                     removePanel.appendChild(btnDiv);
-                }
-            }
-        },
-
-        getObject(list, id){
-            var pos = list.map(function(e) {
-                return e.id;
-            }).indexOf(id);
-            return list[pos];
+                })
+            })
         },
 
         addTarget: function(e){
             var _this = this;
             var aimId = $(e.currentTarget).attr("aimId");
             // just create a default Target
-            var target = new GDSEModel(
+            var target = this.targets[aimId].create(
                 {
-                "aim": aimId,
-                "impact_category": _this.impactcategories[0].id,
-                "target_value": _this.targetvalues[0].id,
-                "spatial_reference": _this.spatial[0].id
+                    "aim": aimId,
+                    "impact_category": _this.impactcategories.first().id,
+                    "target_value": _this.targetvalues.first().id,
+                    "spatial_reference": _this.spatial.first().id
                 },
-                { apiTag: 'targets', apiIds: [_this.caseStudy.id] }
+                {
+                    wait: true,
+                    success: function(){
+                        _this.render();
+                    },
+                    error: _this.onError
+                }
             );
-            target.save(null, {
-                success: function(){
-                    var temp = _this.targets;
-                    temp.push({
-                        "id": target.get('id'),
-                        "aim": target.get('aim'),
-                        "impact_category": target.get('impact_category'),
-                        "target_value": target.get('target_value'),
-                        "spatial_reference": target.get('spatial_reference'),
-                        "user": target.get('user')
-                    });
-                    _this.targets = _.sortBy(temp, 'aim' );
-                    _this.render();
-                },
-                error: _this.onError
-            });
         },
 
         editTarget: function(e){
@@ -256,23 +214,14 @@ function(_, BaseView, GDSECollection, GDSEModel){
             var select = $(e)[0].target;
             var type = select.getAttribute("type");
             var targetId = parseInt(select.getAttribute("targetId"));
-            var idx = select.options.selectedIndex;
-            var optionId = parseInt(select.options[idx].getAttribute("id"));
-            var target = new GDSEModel(
-                {id: targetId},
-                { apiTag: 'targets', apiIds: [_this.caseStudy.id] }
-            );
+            var aimId = parseInt(select.getAttribute("aimId"));
+            var optionId = parseInt(select.value);
+            var target = _this.targets[aimId].get(targetId);
             if (type == "targetvalue") {
                 target.save({
                     target_value: optionId
                 }, {
                     patch: true,
-                    success: function(){
-                        var pos = _this.targets.map(function(e) {
-                            return e.id;
-                        }).indexOf(target.get('id'));
-                        _this.targets[pos].target_value = optionId;
-                    },
                     error: _this.onError
                 });
             } else if (type == "impact") {
@@ -280,12 +229,6 @@ function(_, BaseView, GDSECollection, GDSEModel){
                     "impact_category": optionId
                 }, {
                     patch: true,
-                    success: function(){
-                        var pos = _this.targets.map(function(e) {
-                            return e.id;
-                        }).indexOf(target.get('id'));
-                        _this.targets[pos].impact_category = optionId;
-                    },
                     error: _this.onError
                 });
             } else {
@@ -293,12 +236,6 @@ function(_, BaseView, GDSECollection, GDSEModel){
                     "spatial_reference": optionId
                 }, {
                     patch: true,
-                    success: function(){
-                        var pos = _this.targets.map(function(e) {
-                            return e.id;
-                        }).indexOf(target.get('id'));
-                        _this.targets[pos].spatial_reference = optionId;
-                    },
                     error: _this.onError
                 });
             }
@@ -306,20 +243,14 @@ function(_, BaseView, GDSECollection, GDSEModel){
 
         deleteTarget: function(e){
             var _this = this;
-            var select = $(e)[0].target;
-            var targetId = parseInt(select.getAttribute("targetId"));
+            var button = $(e)[0].target;
+            var targetId = parseInt(button.getAttribute("targetId"));
+            var aimId = parseInt(button.getAttribute("aimId"));
             var message = gettext('Do you really want to delete the target?');
             _this.confirm({ message: message, onConfirm: function(){
-                var target = new GDSEModel(
-                    {id: targetId},
-                    { apiTag: 'targets', apiIds: [_this.caseStudy.id] }
-                );
+                var target = _this.targets[aimId].get(targetId);
                 target.destroy({
                     success: function(){
-                        var pos = _this.targets.map(function(e) {
-                            return e.id;
-                        }).indexOf(targetId);
-                        _this.targets.splice(pos, 1);
                         _this.render();
                     },
                     error: _this.onError
