1585011332dc30eeeb61546b84a61dc453cfa213
ChrFr <christoph.franke@web.de>
2018-09-20 15:06:58 +0200

    filter strategies by user and keyflow, create empty one if none exists when querying

--- repair/apps/changes/views/strategies.py
+++ repair/apps/changes/views/strategies.py
@@ -1,4 +1,6 @@
 from repair.apps.utils.views import CasestudyViewSetMixin, ReadUpdateViewSet
+from repair.apps.asmfa.models import KeyflowInCasestudy
+from repair.apps.login.models import UserInCasestudy
 from repair.apps.changes.models import (
     Strategy,
     SolutionInStrategy,
@@ -20,13 +22,35 @@ class StrategyViewSet(CasestudyViewSetMixin,
     serializer_class = StrategySerializer
     queryset = Strategy.objects.all()
 
-    def list(self, request, *args, **kwargs):
+    def get_queryset(self):
+        strategies = Strategy.objects.all()
+        casestudy_pk = self.kwargs.get('casestudy_pk')
+        keyflow_pk = self.kwargs.get('keyflow_pk')
+        if keyflow_pk is not None:
+            strategies = strategies.filter(keyflow__id=keyflow_pk)
+        # if no requested specific user strategies
+        # only retrieve strategies of user
+        if (self.request.user.id and 'user' not in self.request.query_params and
+            'user__in' not in self.request.query_params):
+            strategies = strategies.filter(
+                user__user__user_id=self.request.user.id)
+            # there should always be at least one strategy per user and keyflow
+            # (actually exactly one, but this should be handled by the frontend)
+            if len(strategies) == 0 and keyflow_pk is not None:
+                profile = UserInCasestudy.objects.get(
+                    user=self.request.user.id,
+                    casestudy=casestudy_pk
+                )
+                keyflow = KeyflowInCasestudy.objects.get(id=keyflow_pk)
+                strategy = Strategy(
+                    user=profile,
+                    keyflow=keyflow,
+                    name=''
+                )
+                strategy.save()
+                strategies = Strategy.objects.filter(id=strategy.id)
+        return strategies
 
-        if (request.user.id and 'user' not in request.query_params and
-            'user__in' not in request.query_params):
-            self.queryset = self.queryset.filter(user__user__user_id=request.user.id)
-
-        return super().list(request, *args, **kwargs)
 
 
 class SolutionInStrategyViewSet(CasestudyViewSetMixin,
