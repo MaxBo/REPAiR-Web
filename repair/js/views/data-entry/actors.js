cce091cfc4f244d4c37de2bda47382460afc1c2d
ChrFr <christoph.franke@web.de>
2018-09-25 18:27:01 +0200

    data entry - increased performance for showing actors in table

--- repair/js/views/data-entry/actors.js
+++ repair/js/views/data-entry/actors.js
@@ -94,6 +94,9 @@ var ActorsView = BaseView.extend(
         });
         this.filterSelect = this.el.querySelector('#included-filter-select');
         this.datatable = $('#actors-table').DataTable();
+        $('#actors-table tbody').on('click', 'tr', function () {
+            _this.selectRow(this);
+        });
         this.renderActors();
     },
 
@@ -102,14 +105,11 @@ var ActorsView = BaseView.extend(
             activityId = this.el.querySelector('select[name="activity-filter"]').value;
             data = (activityId =="-1") ? {} : { activity: activityId }
         this.datatable.clear().draw();
-        this.actorRows = [];
         this.loader.activate();
         this.actors.fetch({
             data: data,
             success: function(){
-                _this.actors.each(
-                    function(actor){_this.addActorRow(actor)
-                }); // you have to define function instead of passing this.addActorRow, else scope is wrong
+                _this.addActorRows(_this.actors);
                 _this.loader.deactivate();
             },
             error: function(e){
@@ -133,87 +133,77 @@ var ActorsView = BaseView.extend(
         this.datatable.draw();
     },
 
-    /*
-    * add given actor to table
-    */
-    addActorRow: function(actor){
-        var _this = this;
-
-        var dataRow = this.datatable.row.add([
+    addActorRows: function(actors){
+        var _this = this,
+            dataRows = [];
+        actors.forEach(function(actor){
+            row = [
                 actor.get('name'),
                 actor.get('city'),
                 actor.get('address'),
-            ]).draw(),
-            row = dataRow.node();
-        row.setAttribute('data-included', actor.get('included'));
-        this.actorRows.push(row);
-
-        function selectRow(r){
-            _.each(_this.actorRows, function(row){
-                row.classList.remove('selected');
-            });
-            r.classList.add('selected');
-        }
-
-        function setIncluded(actor){
-            var included = actor.get('included');
-            if (!included){
-                row.classList.add('dsbld');
-                if (!_this.showAll)
-                    row.style.display = "none";
-            } else {
-                row.classList.remove('dsbld')
-                row.style.display = "table-row";
-            }
-            row.setAttribute('data-included', included);
-        };
-        setIncluded(actor);
-
-        // open a view on the actor (showing attributes and locations)
-        function showActor(actor){
-            selectRow(row);
-            actor.caseStudyId = _this.caseStudy.id;
-            actor.keyflowId = _this.model.id;
-            _this.activeActor = actor;
-            if (_this.actorView != null) _this.actorView.close();
-            actor.fetch({ success: function(){
-                _this.actorView = new EditActorView({
-                    el: document.getElementById('edit-actor'),
-                    template: 'edit-actor-template',
-                    model: actor,
-                    activities: _this.activities,
-                    keyflow: _this.model,
-                    onUpload: function(a) {
-                        setIncluded(a);
-                        dataRow.data([
-                            actor.get('name'),
-                            actor.get('city'),
-                            actor.get('address')
-                        ]);
-                        showActor(a);
-                    },
-                    focusarea: _this.caseStudy.get('properties').focusarea,
-                    areaLevels: _this.areaLevels,
-                    reasons: _this.reasons
-                });
-            }})
-        }
-
-        // row is clicked -> open view and remember that this actor is "active"
-        row.addEventListener('click', function() {
-            if (_this.activeActor != actor || actor.id == null){
-                if (_this.actorView != null && _this.actorView.hasChanged()){
-                    var message = gettext('Attributes of the actor have been changed but not uploaded. <br><br>Do you want to discard the changes?');
-                    _this.confirm({
-                        message: message,
-                        onConfirm: function() { showActor(actor) }
-                    })
-                }
-                else showActor(actor);
-            }
+                actor.id
+            ];
+            var dataRow = _this.datatable.row.add(row);
+            dataRows.push(dataRow);
+            _this.setIncluded(actor, dataRow);
         });
+        this.datatable.draw();
+        return dataRows;
+    },
 
-        return dataRow;
+    showActor: function(actor, dataRow){
+        var _this = this;
+        actor.caseStudyId = _this.caseStudy.id;
+        actor.keyflowId = _this.model.id;
+        _this.activeActor = actor;
+        if (_this.actorView != null) _this.actorView.close();
+        actor.fetch({ success: function(){
+            _this.actorView = new EditActorView({
+                el: document.getElementById('edit-actor'),
+                template: 'edit-actor-template',
+                model: actor,
+                activities: _this.activities,
+                keyflow: _this.model,
+                onUpload: function(a) {
+                    _this.setIncluded(a, dataRow);
+                    dataRow.data([
+                        actor.get('name'),
+                        actor.get('city'),
+                        actor.get('address')
+                    ]);
+                    _this.showActor(a, dataRow);
+                },
+                focusarea: _this.caseStudy.get('properties').focusarea,
+                areaLevels: _this.areaLevels,
+                reasons: _this.reasons
+            });
+        }})
+    },
+
+    selectRow: function(row){
+        var _this = this,
+            dataRow = this.datatable.row(row);
+        $("#actors-table tbody tr").removeClass('selected');
+        row.classList.add('selected');
+
+        var data = dataRow.data(),
+            actor = this.actors.get(data[data.length - 1]);
+        this.showActor(actor, dataRow);
+    },
+
+    setIncluded: function(actor, dataRow){
+        var _this = this,
+            included = actor.get('included'),
+            row = dataRow.node();
+        if (!included){
+            row.classList.add('dsbld');
+            if (!_this.showAll)
+                row.style.display = "none";
+        } else {
+            row.classList.remove('dsbld')
+            row.style.display = "table-row";
+        }
+        row.setAttribute('data-included', included);
     },
 
     /*
@@ -240,8 +230,8 @@ var ActorsView = BaseView.extend(
             }, {
                 wait: true,
                 success: function(){
-                    var row = _this.addActorRow(actor);
-                    row.node().click();
+                    var dataRow = _this.addActorRows([actor])[0];
+                    _this.selectRow(dataRow.node());
                 }
             })
         }
