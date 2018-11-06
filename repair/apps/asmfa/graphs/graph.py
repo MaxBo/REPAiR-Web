b0cb5158aff5117683c1559cedbb487bb53e9864
ChrFr <christoph.franke@web.de>
2018-11-06 16:39:13 +0100

    fixed test

--- repair/apps/asmfa/graphs/graph.py
+++ repair/apps/asmfa/graphs/graph.py
@@ -22,13 +22,14 @@ class KeyflowGraph:
         flows = Actor2Actor.objects.filter(origin__in=actors,
                                            destination__in=actors)
         # example iteration over actors in keyflow
-        # (maybe you can avoid iterations at all, kind of slow)
+        # (maybe you can avoid iterations at all, they are VERY slow)
         for actor in actors:
             # location of an actor, django throws errors when none found (actor has no loaction),
             # kind of annoying
             try:
                 location = AdministrativeLocation.objects.get(id=actor.id)
             except:
+                # surprisingly many actors miss a location, shouldn't be that way
                 print('no location for {}'.format(actor.name))
             # i forgot how to make the OR filter
             # flows leaving the actor, you can filter already filtered querysets again
@@ -38,18 +39,19 @@ class KeyflowGraph:
             # flows going to actor
             in_flows = flows.filter(destination=actor)
 
-        # get a composition
-        composition = flows[0].composition
-        fractions = composition.fractions
-        # the fractions relate to the composition, not the other way around,
-        # so a reverse manager is used, that one can't be iterated
-        # you can get the reverse related models this way
-        fractions = fractions.all()
-        fraction = fractions[0]
-        # the material
-        material = fraction.material
-        # the actual fraction of the fraction (great naming here)
-        f = fraction.fraction
+        for flow in flows:
+            # get a composition
+            composition = flow.composition
+            fractions = composition.fractions
+            # the fractions relate to the composition, not the other way around,
+            # so a reverse manager is used, that one can't be iterated
+            # you can get the reverse related models this way
+            fractions = fractions.all()
+            for fraction in fractions:
+                # the material
+                material = fraction.material
+                # the actual fraction of the fraction (great naming here)
+                f = fraction.fraction
 
         return None
 
