d8a6366cf76c5e8e8de08dfbc9f52d3b926bfbb8
ChrFr <christoph.franke@web.de>
2019-01-30 17:28:04 +0100

    sped up flow_count for materials (now counting compositions instead)

--- repair/js/views/data-entry/materials.js
+++ repair/js/views/data-entry/materials.js
@@ -69,11 +69,11 @@ var MaterialsView = BaseView.extend(
     },
 
     materialToNode: function(material){
-        var flowCount = material.get('flow_count');
+        var compCount = material.get('composition_count');
         var node = {
             id: material.id,
             parent: material.get('parent'),
-            text: material.get('name') + ' (' + flowCount + ' ' + gettext('flows') + ')',
+            text: material.get('name') + ' (' + compCount + ' ' + gettext('compositions') + ')',
             model: material,
             state: { collapsed: true }
         };
