cba8976f6003cd4632a73bf4561a830035074b34
ChrFr <christoph.franke@web.de>
2019-01-31 13:27:24 +0100

    seperate model to split flows into it's fractions, auto-generated when creating/updating models

--- repair/apps/asmfa/models/flows.py
+++ repair/apps/asmfa/models/flows.py
@@ -3,7 +3,8 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-from repair.apps.asmfa.models.keyflows import (KeyflowInCasestudy, Composition)
+from repair.apps.asmfa.models.keyflows import (KeyflowInCasestudy, Composition,
+                                               Material, ProductFraction)
 from repair.apps.publications.models import PublicationInCasestudy
 from repair.apps.asmfa.models.nodes import (
     ActivityGroup,
@@ -89,3 +90,65 @@ class Actor2Actor(Flow):
                                     null=True,
                                     on_delete=models.SET_NULL,
                                     related_name='Actor2ActorData')
+
+    def save(self, **kwargs):
+        super().save(**kwargs)
+
+        # delete eventually already translated fraction flows
+        # (recreation in any case)
+        fraction_flows = FractionFlow.objects.filter(flow=self)
+        fraction_flows.delete()
+        composition = self.composition
+        fractions = ProductFraction.objects.filter(composition=composition)
+        for fraction in fractions:
+            fraction_flow = FractionFlow(
+                flow=self,
+                origin=self.origin,
+                destination=self.destination,
+                material=fraction.material,
+                amount=self.amount*fraction.fraction,
+                nace=composition.nace,
+                composition_name=composition.name,
+                publication=fraction.publication or self.publication,
+                avoidable=fraction.avoidable,
+                hazardous=fraction.hazardous,
+                waste=self.waste,
+                process=self.process,
+                keyflow=self.keyflow,
+                description=self.description,
+                year=self.year
+            )
+            fraction_flow.save()
+
+
+class FractionFlow(Flow):
+    # fraction flow can be related to a keyflow (status quo)
+    # but doesn't have to (modified strategy)
+    flow = models.ForeignKey(Actor2Actor,
+                             on_delete=models.CASCADE,
+                             null=True,
+                             related_name='f_flow')
+    destination = models.ForeignKey(Actor,
+                                    on_delete=PROTECT_CASCADE,
+                                    related_name='f_inputs')
+    origin = models.ForeignKey(Actor,
+                               on_delete=PROTECT_CASCADE,
+                               related_name='f_outputs')
+    material = models.ForeignKey(Material,
+                                 on_delete=PROTECT_CASCADE,
+                                 related_name='f_material')
+
+    amount = models.BigIntegerField(default=0) # in kilograms
+
+    nace = models.CharField(max_length=255, blank=True)
+    composition_name = models.CharField(max_length=255, blank=True)
+    publication = models.ForeignKey(PublicationInCasestudy, null=True,
+                                    on_delete=models.SET_NULL,
+                                    related_name='f_pub')
+    avoidable = models.BooleanField(default=True)
+    hazardous = models.BooleanField(default=True)
+
+
+
+
+
