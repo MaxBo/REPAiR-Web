66e9afab366ca3b016d5f186385664e106c63123
Christoph Franke <franke@ggr-planung.de>
2019-04-09 11:36:17 +0200

    Pseudocode for translating graph to db after calculation

--- repair/apps/asmfa/graphs/graph.py
+++ repair/apps/asmfa/graphs/graph.py
@@ -1,5 +1,6 @@
 from repair.apps.asmfa.models import (Actor2Actor, FractionFlow, Actor,
-                                      ActorStock, Material)
+                                      ActorStock, Material,
+                                      StrategyFractionFlow)
 from repair.apps.asmfa.graphs.graphwalker import GraphWalker
 import graph_tool as gt
 from graph_tool import stats as gt_stats
@@ -63,7 +64,7 @@ class BaseGraph:
             Q(id__in=stockflows.values('origin_id'))
         )
         flows = list(chain(actorflows, stockflows))
-        
+
         self.graph = gt.Graph(directed=True)
 
         # Add the actors to the graph
@@ -86,8 +87,8 @@ class BaseGraph:
         # need a persistent edge id, because graph-tool can reindex the edges
         self.graph.edge_properties["id"] = self.graph.new_edge_property("int")
         self.graph.edge_properties['amount'] = self.graph.new_edge_property("int")
-        self.graph.edge_properties['material'] = self.graph.new_edge_property("string")    
-        
+        self.graph.edge_properties['material'] = self.graph.new_edge_property("string")
+
         for i in range(len(flows)):
             # get the start and and actor id's
             v0 = actorids.get(flows[i].origin.id)
@@ -95,7 +96,7 @@ class BaseGraph:
                 v1 = actorids.get(flows[i].destination.id)
             else:
                 v1 = v0
-                
+
             if (v0 != None and v1 != None and isinstance(flows[i], FractionFlow)):
                 e = self.graph.add_edge(
                     self.graph.vertex(v0), self.graph.vertex(v1))
@@ -119,7 +120,7 @@ class BaseGraph:
         for v in self.graph.vertices():
             if(len(v.all_edges()) == 0):
                 invalid.append(v)
-                
+
         if len(invalid) != 0:
             return invalid
         else:
@@ -139,6 +140,7 @@ class BaseGraph:
             flows.append(flow)
         return {'flows':flows}
 
+
 class StrategyGraph(BaseGraph):
     def __init__(self, strategy):
         self.keyflow = strategy.keyflow
@@ -155,9 +157,57 @@ class StrategyGraph(BaseGraph):
             raise FileNotFoundError
         g = base_graph.load()
         gw = GraphWalker(g)
-        self.graph = gw.calculate_solution(2.0)
+        # ToDo: get the solutions in this strategy and order them by
+        # priority
+        for solution in solutions:
+            # ToDo: get the solution part
+            for solution_part in solution_parts:
+                # ToDo: get the implementation and its quantity
+                # ToDo: SolutionInStrategy geometry for filtering?
+                self.graph = gw.calculate_solution(solution, solution_part,
+                                                   quantity)
         self.graph.save(self.filename)
+
+        # ToDo: wipe all existing StrategyFractionFlows with strategy id
+        # (modified ones) and FractionFlows with strategy id (new ones)
+        self.clean()
+
+        # ToDo: put modifications and new flows into database
+        self.translate_to_db()
         return self.graph
+
+    def clean(self):
+        flows = FractionFlow.objects.filter(strategy=self.strategy)
+        flows.destroy() # or sth like that
+        modified = StrategyFractionFlow.objects.filter(strategy=self.strategy)
+        modified.destroy()
+
+    def translate_to_db(self):
+
+        # ToDo: get edges from self.graph with no fractionflow id
+        for edge in edges:
+            origin = Actor.objects.get(edge.originid)
+            new_flow = FractionFlow(origin=origin, destination=destination,
+                                    amount=edge.amount,
+                                    strategy=self.strategy,
+                                    # ToDo: all other attributes like material,
+                                    # maybe get some of those from the flow the
+                                    # new edge was derived from
+                                    )
+
+        # ToDo: get modified flows from self.graph:
+        for edge in modified_edges:
+            mod_flow = StrategyFractionFlow(fractionflow=edge.fractionflowid,
+                                            amount=edge.amount,
+                                            strategy=self.strategy)
+
+        # ToDo: deleted flows? keep track of those (ids are sufficient) somehow
+        # or set amounts to zero in graph instead of actual deleting (then you
+        # don't need this part, the one before would deal with it)
+        for del_id in deleted_flow_ids:
+            mod_flow = StrategyFractionFlow(fractionflow=del_id,
+                                            amount=0,
+                                            strategy=self.strategy)
 
     def to_queryset(self):
         if not self.graph:
