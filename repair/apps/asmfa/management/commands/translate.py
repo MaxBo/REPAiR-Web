99b71e04882f66012e3218e95bf6fcd789284d0b
ChrFr <christoph.franke@web.de>
2019-04-25 15:05:38 +0200

    translate to fraction-flows: arguments for ignoring stocks/flows

--- repair/apps/asmfa/management/commands/translate.py
+++ repair/apps/asmfa/management/commands/translate.py
@@ -5,39 +5,43 @@ from repair.apps.asmfa.models import Actor2Actor, ActorStock
 from repair.apps.login.models import CaseStudy
 
 
-def translate(*args, casestudy_id=None):
+def translate(*args, casestudy_id=None,
+              ignore_stocks=False, ignore_flows=False):
     casestudy_ids = CaseStudy.objects.all().values_list('id', flat=True) \
         if casestudy_id is None else [casestudy_id]
 
     for casestudy_id in sorted(casestudy_ids):
         print('-' * 50)
         print(f'Processing Casestudy id {casestudy_id}')
-        # convert stocks
-        stocks = ActorStock.objects.filter(keyflow__casestudy__id=casestudy_id)
-        length = len(stocks)
-        print('\nTranslating actor-stocks to fraction-flows')
         start = time.time()
-        for i, stock in enumerate(stocks):
-            stock.save()
-            if i % 10 == 0:
-                end = time.time()
-                t = end - start
-                print(f'{i}/{length} stocks converted in {t}s')
-                start = time.time()
-        print('done')
+
+        # convert stocks
+        if not ignore_stocks:
+            stocks = ActorStock.objects.filter(keyflow__casestudy__id=casestudy_id)
+            length = len(stocks)
+            print('\nTranslating actor-stocks to fraction-flows')
+            for i, stock in enumerate(stocks):
+                stock.save()
+                if i % 10 == 0:
+                    end = time.time()
+                    t = end - start
+                    print(f'{i}/{length} stocks converted in {t}s')
+                    start = time.time()
+            print('done')
 
         # convert flows
-        flows = Actor2Actor.objects.filter(keyflow__casestudy__id=casestudy_id)
-        length = len(flows)
-        print('\nTranslating actor2actor-flows to fraction-flows')
-        for i, flow in enumerate(flows):
-            flow.save()
-            if i % 10 == 0:
-                end = time.time()
-                t = end - start
-                print(f'{i}/{length} flows converted in {t}s')
-                start = time.time()
-        print('done')
+        if not ignore_flows:
+            flows = Actor2Actor.objects.filter(keyflow__casestudy__id=casestudy_id)
+            length = len(flows)
+            print('\nTranslating actor2actor-flows to fraction-flows')
+            for i, flow in enumerate(flows):
+                flow.save()
+                if i % 10 == 0:
+                    end = time.time()
+                    t = end - start
+                    print(f'{i}/{length} flows converted in {t}s')
+                    start = time.time()
+            print('done')
 
 
 class Command(BaseCommand):
@@ -46,12 +50,20 @@ class Command(BaseCommand):
 
     def add_arguments(self, parser):
         parser.add_argument('--casestudy_id', action='append', type=int)
+        parser.add_argument('--flows_only', action='store_true')
+        parser.add_argument('--stocks_only', action='store_true')
 
     def handle(self, *args, **options):
         casestudy_ids = options['casestudy_id']
+        flows_only = options['flows_only']
+        stocks_only = options['stocks_only']
+        if flows_only and stocks_only:
+            raise Exception("flows_only excludes stocks_only")
         if casestudy_ids:
             for casestudy_id in casestudy_ids:
-                translate(casestudy_id=casestudy_id)
+                translate(casestudy_id=casestudy_id,
+                          ignore_flows=stocks_only,
+                          ignore_stocks=flows_only)
         else:
             translate()
 
