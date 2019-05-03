cae6162b4378d398f495782f6d16719723ba1ff8
Christoph Franke <franke@ggr-planung.de>
2019-05-03 15:24:58 +0200

    fixed stock filter

--- repair/apps/asmfa/views/flowfilter.py
+++ repair/apps/asmfa/views/flowfilter.py
@@ -129,14 +129,6 @@ class FilterFlowViewSet(PostGetViewMixin, RevisionMixin,
     def get_queryset(self):
         keyflow_pk = self.kwargs.get('keyflow_pk')
         flows = FractionFlow.objects.filter(keyflow__id=keyflow_pk)
-        # rename filter for stock, as this relates to field with stock pk
-        # but serializer returns boolean in this field (if it is stock)
-        tmp = self.request.query_params._mutable
-        self.request.query_params._mutable = True
-        stock_filter = self.request.query_params.pop('stock', None)
-        if stock_filter is not None:
-            self.request.query_params['to_stock'] = stock_filter[0]
-        self.request.query_params._mutable = tmp
         return flows.order_by('origin', 'destination')
 
     # POST is used to send filter parameters not to create
@@ -245,6 +237,11 @@ class FilterFlowViewSet(PostGetViewMixin, RevisionMixin,
         if query_params:
             query_params = query_params.copy()
             strategy = query_params.pop('strategy', None)
+            # rename filter for stock, as this relates to field with stock pk
+            # but serializer returns boolean in this field (if it is stock)
+            stock_filter = query_params.pop('stock', None)
+            if stock_filter is not None:
+                query_params['to_stock'] = stock_filter[0]
         queryset = super()._filter(lookup_args, query_params=query_params,
                                    SerializerClass=SerializerClass)
         if strategy:
