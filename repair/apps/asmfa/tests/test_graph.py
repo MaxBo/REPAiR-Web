61716d0c6c4b4fc4ba483789bcd1c1cfe13f3c6f
Tom Commandeur <t.j.f.commandeur@tudelft.nl>
2019-07-04 18:41:18 +0200

    uncomment GraphTest

--- repair/apps/asmfa/tests/test_graph.py
+++ repair/apps/asmfa/tests/test_graph.py
@@ -24,29 +24,29 @@ from repair.apps.login.factories import UserInCasestudyFactory
 from django.contrib.gis.geos import Polygon, Point, GeometryCollection
 from django.db.models.functions import Coalesce
 
-#class GraphTest(LoginTestCase, APITestCase):
+class GraphTest(LoginTestCase, APITestCase):
 
-    #@classmethod
-    #def setUpClass(cls):
-        #super().setUpClass()
+    @classmethod
+    def setUpClass(cls):
+        super().setUpClass()
 
-    #def setUp(self):
-        #super().setUp()
-        #self.activitygroup1 = ActivityGroupFactory(name='MyGroup',
-                                                   #keyflow=self.kic)
-        #self.activitygroup2 = ActivityGroupFactory(name='AnotherGroup',
-                                                   #keyflow=self.kic)
-        #self.activity1 = ActivityFactory(nace='NACE1',
-                                         #activitygroup=self.activitygroup1)
-        #self.activity2 = ActivityFactory(nace='NACE2',
-                                         #activitygroup=self.activitygroup1)
-        #self.activity3 = ActivityFactory(nace='NACE1',
-                                         #activitygroup=self.activitygroup1)
-        #self.activity4 = ActivityFactory(nace='NACE3',
-                                         #activitygroup=self.activitygroup2)
+    def setUp(self):
+        super().setUp()
+        self.activitygroup1 = ActivityGroupFactory(name='MyGroup',
+                                                   keyflow=self.kic)
+        self.activitygroup2 = ActivityGroupFactory(name='AnotherGroup',
+                                                   keyflow=self.kic)
+        self.activity1 = ActivityFactory(nace='NACE1',
+                                         activitygroup=self.activitygroup1)
+        self.activity2 = ActivityFactory(nace='NACE2',
+                                         activitygroup=self.activitygroup1)
+        self.activity3 = ActivityFactory(nace='NACE1',
+                                         activitygroup=self.activitygroup1)
+        self.activity4 = ActivityFactory(nace='NACE3',
+                                         activitygroup=self.activitygroup2)
 
-    #def test_graph(self):
-        #self.graph = BaseGraph(self.kic)
+    def test_graph(self):
+        self.graph = BaseGraph(self.kic)
 
 class StrategyGraphTest(LoginTestCase, APITestCase):
     stakeholdercategoryid = 48
