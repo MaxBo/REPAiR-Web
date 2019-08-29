5bcc2b8d2029b69529b75d36aed017ba5ef63fb5
ChrFr <christoph.franke@web.de>
2019-08-29 14:02:18 +0200

    button reset objectives ranking

--- repair/js/views/targets/ranking-objectives.js
+++ repair/js/views/targets/ranking-objectives.js
@@ -3,155 +3,163 @@ define(['underscore','views/common/baseview', 'collections/gdsecollection',
         'muuri'],
 
 function(_, BaseView, GDSECollection, Muuri){
+/**
+*
+* @author Christoph Franke
+* @name module:views/RankingObjectivesView
+* @augments Backbone.View
+*/
+var RankingObjectivesView = BaseView.extend(
+    /** @lends module:views/RankingObjectivesView.prototype */
+    {
+
     /**
+    * render workshop view on ranking the objectives in a keyflow
     *
-    * @author Christoph Franke
-    * @name module:views/RankingObjectivesView
-    * @augments Backbone.View
+    * @param {Object} options
+    * @param {HTMLElement} options.el                      element the view will be rendered in
+    * @param {string} options.template                     id of the script element containing the underscore template to render this view
+    * @param {module:models/CaseStudy} options.caseStudy   the casestudy of the keyflow
+    * @param {module:models/CaseStudy} options.keyflowId   the keyflow the objectives belong to
+    *
+    * @constructs
+    * @see http://backbonejs.org/#View
     */
-    var RankingObjectivesView = BaseView.extend(
-        /** @lends module:views/RankingObjectivesView.prototype */
-        {
+    initialize: function(options){
+        RankingObjectivesView.__super__.initialize.apply(this, [options]);
+        var _this = this;
+        _.bindAll(this, 'renderObjective');
 
-        /**
-        * render workshop view on ranking the objectives in a keyflow
-        *
-        * @param {Object} options
-        * @param {HTMLElement} options.el                      element the view will be rendered in
-        * @param {string} options.template                     id of the script element containing the underscore template to render this view
-        * @param {module:models/CaseStudy} options.caseStudy   the casestudy of the keyflow
-        * @param {module:models/CaseStudy} options.keyflowId   the keyflow the objectives belong to
-        *
-        * @constructs
-        * @see http://backbonejs.org/#View
-        */
-        initialize: function(options){
-            RankingObjectivesView.__super__.initialize.apply(this, [options]);
-            var _this = this;
-            _.bindAll(this, 'renderObjective');
+        this.template = options.template;
+        this.caseStudy = options.caseStudy;
+        this.keyflowId = options.keyflowId;
+        this.keyflowName = options.keyflowName;
+        this.userObjectives = options.userObjectives;
+        this.aims = options.aims;
 
-            this.template = options.template;
-            this.caseStudy = options.caseStudy;
-            this.keyflowId = options.keyflowId;
-            this.keyflowName = options.keyflowName;
-            this.userObjectives = options.userObjectives;
-            this.aims = options.aims;
+        this.render();
+    },
 
-            this.render();
-        },
+    /*
+    * dom events (managed by jquery)
+    */
+    events: {
+        'click .reset': 'reset'
+    },
 
-        /*
-        * dom events (managed by jquery)
-        */
-        events: {
-            'click .add-target': 'addTarget'
-        },
+    /*
+    * render the view
+    */
+    render: function(){
+        var _this = this,
+            html = document.getElementById(this.template).innerHTML,
+            template = _.template(html),
+            title;
+        if (this.keyflowId == -1)
+            title = gettext("Ranking general objectives")
+        else
+            title = gettext("Ranking objectives for the keyflow ") + "<i>" + this.keyflowName + "</i>";
+        this.el.innerHTML = template({ title: title });
+        var panel = this.el.querySelector('.item-panel');
+        this.grid = new Muuri(panel, {
+            items: '.panel-item',
+            dragAxis: 'y',
+            layoutDuration: 400,
+            layoutEasing: 'ease',
+            dragEnabled: true,
+            dragSortInterval: 0,
+            dragReleaseDuration: 400,
+            dragReleaseEasing: 'ease'
+        });
 
-        /*
-        * render the view
-        */
-        render: function(){
-            var _this = this,
-                html = document.getElementById(this.template).innerHTML,
-                template = _.template(html),
-                title;
-            if (this.keyflowId == -1)
-                title = gettext("Ranking general objectives")
-            else
-                title = gettext("Ranking objectives for the keyflow ") + "<i>" + this.keyflowName + "</i>";
-            this.el.innerHTML = template({ title: title });
-            var panel = this.el.querySelector('.item-panel');
-            this.grid = new Muuri(panel, {
-                items: '.panel-item',
-                dragAxis: 'y',
-                layoutDuration: 400,
-                layoutEasing: 'ease',
-                dragEnabled: true,
-                dragSortInterval: 0,
-                dragReleaseDuration: 400,
-                dragReleaseEasing: 'ease'
+        this.grid.on('dragReleaseEnd', function(item) {
+            var id = item.getElement().dataset['id'];
+            _this.uploadPriorities(id);
+        });
+        // render objectives with set priorities on top
+        this.userObjectives.forEach(function(objective){
+            if (objective.get('priority') >= 0)
+                _this.renderObjective(objective)
+        });
+        this.userObjectives.forEach(function(objective){
+            if (objective.get('priority') < 0)
+                _this.renderObjective(objective)
+        });
+
+        var btns = this.el.querySelectorAll('button:not(.reset)');
+        _.each(btns, function(button){
+            button.style.display = 'none';
+        });
+    },
+
+    reset: function(){
+        this.userObjectives.forEach(function(objective){
+            objective.set('priority', -1);
+        });
+        this.uploadPriorities();
+        this.render();
+    },
+
+    renderObjective: function(objective){
+        var html = document.getElementById('panel-item-template').innerHTML,
+            template = _.template(html),
+            panelItem = document.createElement('div'),
+            itemContent = document.createElement('div'),
+            priority = objective.get('priority'),
+            _this = this;
+        var aim = this.aims.get(objective.get('aim'));
+        panelItem.classList.add('panel-item');
+        panelItem.classList.add('draggable');
+        panelItem.style.position = 'absolute';
+        panelItem.dataset.id = objective.id;
+        itemContent.classList.add('noselect', 'item-content');
+        itemContent.innerHTML = template({ name: aim.get('text') });
+        panelItem.appendChild(itemContent);
+        this.grid.add(panelItem);
+
+        if (priority < 1)
+            panelItem.style.background = '#d1d1d1';
+        else {
+            var overlay = panelItem.querySelector('.overlay');
+            overlay.style.display = 'inline-block';
+            overlay.innerHTML = '#' + priority;
+        }
+        var desc = aim.get('description');
+        if (desc){
+            $(panelItem).popover({
+                trigger: "hover",
+                container: 'body',
+                content: desc.replace(/\n/g, "<br/>"),
+                html: true
             });
+        }
+    },
 
-            this.grid.on('dragReleaseEnd', function(item) {
-                var id = item.getElement().dataset['id'];
-                _this.uploadPriorities(id);
-            });
-            // render objectives with set priorities on top
-            this.userObjectives.forEach(function(objective){
-                if (objective.get('priority') >= 0)
-                    _this.renderObjective(objective)
-            });
-            this.userObjectives.forEach(function(objective){
-                if (objective.get('priority') < 0)
-                    _this.renderObjective(objective)
-            });
-
-            var btns = this.el.querySelectorAll('button');
-            _.each(btns, function(button){
-                button.style.display = 'none';
-            });
-        },
-
-        renderObjective: function(objective){
-            var html = document.getElementById('panel-item-template').innerHTML,
-                template = _.template(html),
-                panelItem = document.createElement('div'),
-                itemContent = document.createElement('div'),
-                priority = objective.get('priority'),
-                _this = this;
-            var aim = this.aims.get(objective.get('aim'));
-            panelItem.classList.add('panel-item');
-            panelItem.classList.add('draggable');
-            panelItem.style.position = 'absolute';
-            panelItem.dataset.id = objective.id;
-            itemContent.classList.add('noselect', 'item-content');
-            itemContent.innerHTML = template({ name: aim.get('text') });
-            panelItem.appendChild(itemContent);
-            this.grid.add(panelItem);
-
-            if (priority < 1)
-                panelItem.style.background = '#d1d1d1';
-            else {
-                var overlay = panelItem.querySelector('.overlay');
+    uploadPriorities: function(draggedId){
+        var items = this.grid.getItems(),
+            priority = 1,
+            _this = this;
+        items.forEach(function(item){
+            var el = item.getElement(),
+                id = el.dataset.id,
+                objective = _this.userObjectives.get(id);
+            // only update priorities for the dragged item and those whose
+            // priority was assigned before
+            if (draggedId == id || parseInt(objective.get('priority')) >= 1) {
+                el.style.background = null;
+                objective.set('priority', priority);
+                objective.save();
+                var overlay = el.querySelector('.overlay');
                 overlay.style.display = 'inline-block';
                 overlay.innerHTML = '#' + priority;
+                priority++;
             }
-            var desc = aim.get('description');
-            if (desc){
-                $(panelItem).popover({
-                    trigger: "hover",
-                    container: 'body',
-                    content: desc.replace(/\n/g, "<br/>"),
-                    html: true
-                });
-            }
-        },
+        })
+        _this.userObjectives.sort();
+        //_this.userObjectives.trigger('priorities-changed')
+    }
 
-        uploadPriorities: function(draggedId){
-            var items = this.grid.getItems(),
-                priority = 1,
-                _this = this;
-            items.forEach(function(item){
-                var el = item.getElement(),
-                    id = el.dataset.id,
-                    objective = _this.userObjectives.get(id);
-                // only update priorities for the dragged item and those whose
-                // priority was assigned before
-                if (draggedId == id || parseInt(objective.get('priority')) >= 1) {
-                    el.style.background = null;
-                    objective.set('priority', priority);
-                    objective.save();
-                    var overlay = el.querySelector('.overlay');
-                    overlay.style.display = 'inline-block';
-                    overlay.innerHTML = '#' + priority;
-                    priority++;
-                }
-            })
-            _this.userObjectives.sort();
-            //_this.userObjectives.trigger('priorities-changed')
-        }
-
-    });
-    return RankingObjectivesView;
+});
+return RankingObjectivesView;
 }
 );
